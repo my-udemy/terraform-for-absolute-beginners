b8188096238cbeed0fab0c6ea2b9e94eec191150
German Vargas <german.vargas@episource.com>
2024-06-28 15:59:11 -0500

    adding docs

--- 072-s3-with-terraform-lab/main.tf
+++ 072-s3-with-terraform-lab/main.tf
@@ -1,4 +1,5 @@
 
+# create finance-bucket resource
 resource "aws_s3_bucket" "finance-bucket" {
   bucket        = "finance-2024"
   force_destroy = true
@@ -7,21 +8,27 @@ resource "aws_s3_bucket" "finance-bucket" {
   }
 }
 
+# upload an object to finance-bucket
 resource "aws_s3_bucket_object" "payments-file-upload" {
   source = "payments.csv"
   key    = "payments"
   bucket = aws_s3_bucket.finance-bucket.bucket
 }
 
+# create a data source for finance-analysts security group
+# since this group is not managed by terraform
+# because it was created with the CLI
 data "aws_iam_group" "finance-analysts-group-data" {
   group_name = "finance-analysts"
 }
 
+# create pepe user
 resource "aws_iam_user" "pepe-user" {
   name          = "pepe"
   force_destroy = true
 }
 
+# add pepe to finance-analysts security group
 resource "aws_iam_user_group_membership" "pepe-membership" {
   user = aws_iam_user.pepe-user.name
   groups = [
@@ -29,6 +36,9 @@ resource "aws_iam_user_group_membership" "pepe-membership" {
   ]
 }
 
+# create a IAM policy called FinantialPolicy
+# which allows all actions on finance-2024 bucket
+# and allows to list all buckets
 resource "aws_iam_policy" "finance-2024-policy" {
   name        = "FinantialPolicy"
   path        = "/"
@@ -54,6 +64,7 @@ resource "aws_iam_policy" "finance-2024-policy" {
   })
 }
 
+# attach finance-2024-policy to finance-analysts security group
 resource "aws_iam_policy_attachment" "finance-2024-policy-attachment" {
   name       = "finance-2024-policy-attachment"
   groups     = [data.aws_iam_group.finance-analysts-group-data.group_name]
